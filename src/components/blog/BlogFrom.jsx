60d978c8957040d28178e3244bd253aef01d5b59
hmjuel78 <hmjuel.web.citk@gmail.com>
2024-09-20 07:00:55 +0600

    blog form start

--- src/components/blog/BlogFrom.jsx
+++ src/components/blog/BlogFrom.jsx
@@ -1,15 +1,21 @@
 import { useState } from "react"
+import { useSelector } from "react-redux"
+import { categorySelector } from "../../features/category/categorySlice"
 
 const BlogFrom = (props) => {
     const { editableBlog } = props
     const [blogTitle, setBlogTitle] = useState('')
     const [blogBody, setBlogBody] = useState('')
+    const [selectCategory, setSelectCategory] = useState('')
+    const { categories } = useSelector(categorySelector)
 
     const changeHandleBlog = (e) => {
         if (e.target.name === 'blogTitle') {
             setBlogTitle(e.target.value)
         } else if (e.target.name === 'blogBody') {
             setBlogBody(e.target.value)
+        } else if (e.target.name === 'selectCategory') {
+            setSelectCategory(e.target.value)
         }
     }
     const blogOnSubmit = (e) => {
@@ -21,6 +27,19 @@ const BlogFrom = (props) => {
     return (
         <div>
             <form onSubmit={blogOnSubmit} className="space-y-4">
+                <select
+                    onChange={changeHandleBlog}
+                    value={selectCategory}
+                    name="selectCategory"
+                    className="select select-bordered w-full"
+                >
+                    {
+                        categories &&
+                        categories?.map(category => (
+                            <option key={category.id} value={category.id}>{category.name}</option>
+                        ))
+                    }
+                </select>
                 <input
                     onChange={changeHandleBlog}
                     value={blogTitle}
